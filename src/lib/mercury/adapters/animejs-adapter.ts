104373e1544fa02dc75e7628c96db312956891a9
Álex Ávila <alexavila0128@gmail.com>
2024-11-19 13:23:30 +0100

    animejs commented until V4 release

--- src/lib/mercury/adapters/animejs-adapter.ts
+++ src/lib/mercury/adapters/animejs-adapter.ts
@@ -1,65 +1,69 @@
-// src/adapters/animeAdapter.ts
-import type {
-  AnimationEngine,
-  EasingFunction,
-} from '../animation-interface.js';
-import {
-    Animation,
-    engine,
-	animate as animeAnimate,
-	type AnimationParams
-} from '@juliangarnierorg/anime-beta';
-import { mergeParams } from '../utils.svelte.js';
 
-function mapEasing(easing?: EasingFunction): string | ((...args: any[]) => number) | undefined {
-  if (!easing) return undefined;
+// This works but waiting for V4 to be released so that people don't need to be a sponsor
 
-  if (typeof easing === 'string') {
-    // Directly use the easing string if it's one of Anime.js's built-in easings
-    return easing;
-  } else if (typeof easing === 'object') {
-    // Handle complex easing types
-    if (easing.type === 'spring') {
-      // Anime.js doesn't have a spring easing, but you can mimic it or return a default
-      // Alternatively, you could throw an error or warning
-      return 'spring(1, 100, 10, 0)';
-    } else if (easing.type === 'cubic-bezier') {
-      return `cubicBezier(${easing.x1}, ${easing.y1}, ${easing.x2}, ${easing.y2})`;
-    } else if (easing.type === 'steps') {
-      return `steps(${easing.steps})`;
-    } else {
-      // Handle other custom types or return a default
-      return 'linear';
-    }
-  }
 
-  // Default easing
-  return 'linear';
-}
+// // src/adapters/animeAdapter.ts
+// import type {
+//   AnimationEngine,
+//   EasingFunction,
+// } from '../animation-interface.js';
+// import {
+//     Animation,
+//     engine,
+// 	animate as animeAnimate,
+// 	type AnimationParams
+// } from '@juliangarnierorg/anime-beta';
+// import { mergeParams } from '../utils.svelte.js';
 
-export const AnimeAdapter: AnimationEngine = {
-  animate(targets, params) {
-    // Map common parameters to Anime.js parameters
-    const { initial, animate: animateAttrs, transition } = params;
-    const mergedParams = mergeParams({ initial, animate: animateAttrs})
-    const mappedEasing = mapEasing(transition?.easing);
+// function mapEasing(easing?: EasingFunction): string | ((...args: any[]) => number) | undefined {
+//   if (!easing) return undefined;
 
-    const animeParams: AnimationParams = {
-      ...mergedParams,
-      duration: transition?.duration ? transition.duration : undefined,
-      delay: transition?.delay ? transition.delay : undefined,
-      ease:mappedEasing,
-      ...transition,
-    };
+//   if (typeof easing === 'string') {
+//     // Directly use the easing string if it's one of Anime.js's built-in easings
+//     return easing;
+//   } else if (typeof easing === 'object') {
+//     // Handle complex easing types
+//     if (easing.type === 'spring') {
+//       // Anime.js doesn't have a spring easing, but you can mimic it or return a default
+//       // Alternatively, you could throw an error or warning
+//       return 'spring(1, 100, 10, 0)';
+//     } else if (easing.type === 'cubic-bezier') {
+//       return `cubicBezier(${easing.x1}, ${easing.y1}, ${easing.x2}, ${easing.y2})`;
+//     } else if (easing.type === 'steps') {
+//       return `steps(${easing.steps})`;
+//     } else {
+//       // Handle other custom types or return a default
+//       return 'linear';
+//     }
+//   }
 
-    const animation: Animation = animeAnimate(targets,animeParams);
-    engine.timeUnit = 's'
-    return {
-      play: () => animation.play(),
-      pause: () => animation.pause(),
-      cancel: () => animation.restart(), // Anime.js doesn't have a cancel method, so we can restart
-      then: (callback) => animation.then(callback),
-      completed: animation.completed,
-    };
-  },
-};
+//   // Default easing
+//   return 'linear';
+// }
+
+// export const AnimeAdapter: AnimationEngine = {
+//   animate(targets, params) {
+//     // Map common parameters to Anime.js parameters
+//     const { initial, animate: animateAttrs, transition } = params;
+//     const mergedParams = mergeParams({ initial, animate: animateAttrs})
+//     const mappedEasing = mapEasing(transition?.easing);
+
+//     const animeParams: AnimationParams = {
+//       ...mergedParams,
+//       duration: transition?.duration ? transition.duration : undefined,
+//       delay: transition?.delay ? transition.delay : undefined,
+//       ease:mappedEasing,
+//       ...transition,
+//     };
+
+//     const animation: Animation = animeAnimate(targets,animeParams);
+//     engine.timeUnit = 's'
+//     return {
+//       play: () => animation.play(),
+//       pause: () => animation.pause(),
+//       cancel: () => animation.restart(), // Anime.js doesn't have a cancel method, so we can restart
+//       then: (callback) => animation.then(callback),
+//       completed: animation.completed,
+//     };
+//   },
+// };
--- src/lib/mercury/utils.svelte.ts
+++ src/lib/mercury/utils.svelte.ts
@@ -1,86 +1,83 @@
-import type { MercuryParams } from './mercury.svelte.js';
-import { type AnimationParams, Scope, type ScopeParams, type DOMTargetSelector  } from '@juliangarnierorg/anime-beta';
-import { untrack } from 'svelte';
-import type { AnimationAttributes, AnimationCallbacks, AnimationControls } from './animation-interface.js';
+import type { AnimationAttributes, AnimationCallbacks, AnimationControls, AnimationParams } from './animation-interface.js';
 
-export default function createEventListeners(
-	node: Node,
-	params: MercuryParams,
-	updateAnimation: (node: HTMLElement, params: AnimationParams) => void
-) {
-	const { whileHover, whileTap, initial, animate } = params;
-	// Store handler functions as named functions
-	const handleEnter = (enterParams: MercuryParams | undefined) => {
-		if (!enterParams) {
-			return;
-		}
-		updateAnimation(node as HTMLElement, enterParams);
-	};
+// export default function createEventListeners(
+// 	node: Node,
+// 	params: AnimationParams,
+// 	updateAnimation: (node: HTMLElement, params: AnimationParams) => void
+// ) {
+// 	const { whileHover, whileTap, initial, animate } = params;
+// 	// Store handler functions as named functions
+// 	const handleEnter = (enterParams: AnimationParams | undefined) => {
+// 		if (!enterParams) {
+// 			return;
+// 		}
+// 		updateAnimation(node as HTMLElement, enterParams);
+// 	};
 
-	const handleOut = () => {
-		if (initial) {
-			updateAnimation(node as HTMLElement, animate);
-		}
-	};
+// 	const handleOut = () => {
+// 		if (initial) {
+// 			updateAnimation(node as HTMLElement, animate);
+// 		}
+// 	};
 
-	// Add the listeners
-	if (whileHover) {
-		node.addEventListener('mouseover', () => handleEnter(whileHover));
-		node.addEventListener('mouseout', handleOut);
-	}
-	if (whileTap) {
-		node.addEventListener('mousedown', () => handleEnter(whileTap));
-		node.addEventListener('mouseup', handleOut);
-	}
+// 	// Add the listeners
+// 	if (whileHover) {
+// 		node.addEventListener('mouseover', () => handleEnter(whileHover));
+// 		node.addEventListener('mouseout', handleOut);
+// 	}
+// 	if (whileTap) {
+// 		node.addEventListener('mousedown', () => handleEnter(whileTap));
+// 		node.addEventListener('mouseup', handleOut);
+// 	}
 
-	// Return the handlers so they can be used to remove listeners
-	return {
-		remove: () => {
-			node.removeEventListener('mouseover', () => handleEnter(whileHover));
-			node.removeEventListener('mouseout', handleOut);
-		}
-	};
-}
+// 	// Return the handlers so they can be used to remove listeners
+// 	return {
+// 		remove: () => {
+// 			node.removeEventListener('mouseover', () => handleEnter(whileHover));
+// 			node.removeEventListener('mouseout', handleOut);
+// 		}
+// 	};
+// }
 
-export function useScope(params: ScopeParams = {}) {
-	let root = $state<DOMTargetSelector | undefined>(params.root);
-	let scope = $state<Scope>();
+// export function useScope(params: ScopeParams = {}) {
+// 	let root = $state<DOMTargetSelector | undefined>(params.root);
+// 	let scope = $state<Scope>();
 
-	$effect(() => {
-		if (!root) return;
-		return untrack(() => {
-			scope = new Scope({ ...params, root });
-			return () => {
-				scope?.revert();
-				scope = undefined;
-			};
-		});
-	});
+// 	$effect(() => {
+// 		if (!root) return;
+// 		return untrack(() => {
+// 			scope = new Scope({ ...params, root });
+// 			return () => {
+// 				scope?.revert();
+// 				scope = undefined;
+// 			};
+// 		});
+// 	});
 
-	return {
-		get root() {
-			return root;
-		},
-		set root(value) {
-			root = value;
-		},
-		get scope() {
-			return scope;
-		},
-		add(...props: Parameters<Scope['add']>) {
-			$effect(() => {
-				if (!scope) return;
-				return untrack(() => {
-					scope?.add(...props);
-				});
-			});
-			return this;
-		},
-		refresh() {
-			scope?.refresh();
-		}
-	};
-}
+// 	return {
+// 		get root() {
+// 			return root;
+// 		},
+// 		set root(value) {
+// 			root = value;
+// 		},
+// 		get scope() {
+// 			return scope;
+// 		},
+// 		add(...props: Parameters<Scope['add']>) {
+// 			$effect(() => {
+// 				if (!scope) return;
+// 				return untrack(() => {
+// 					scope?.add(...props);
+// 				});
+// 			});
+// 			return this;
+// 		},
+// 		refresh() {
+// 			scope?.refresh();
+// 		}
+// 	};
+// }
 
 // Helper Function
 export function mergeParams({
