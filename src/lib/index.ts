9cf324ad249de3d6a40ceabd3bc56dfcd9f498e1
Álex Ávila <alexavila0128@gmail.com>
2025-05-19 19:38:21 +0200

    refactor: removed old typings

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,8 +1,2 @@
 // Reexport your entry components here
 export { mercury } from './mercury/mercury.svelte.js';
-export {
-	type MercuryAttributes,
-	type MercuryExitParams
-} from './mercury/mercury.svelte.js';
-export { animateExit } from './mercury/mercury.svelte.js';
-export { ExitMode } from './mercury/mercury.svelte.js';
