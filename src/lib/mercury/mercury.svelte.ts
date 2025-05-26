a3e72076511f5ff1771226bf64040fe4a41daf8c
Álex Ávila <alexavila0128@gmail.com>
2025-05-26 23:00:29 +0200

    feat: Motion as default engine

--- src/lib/mercury/mercury.svelte.ts
+++ src/lib/mercury/mercury.svelte.ts
@@ -1,9 +1,9 @@
-import {  AnimeEngine, GSAPEngine, MotionEngine } from './adapters/index.js';
+import { MotionEngine } from './adapters/index.js';
 import type { AnimationEngine, AnimationParams } from './animation-interface.js';
 import { handleGestures } from './gestures.js';
 
 export const mercury = (options?: AnimationParams) => {
-	const engine: AnimationEngine = options?.engine ?? AnimeEngine;
+	const engine: AnimationEngine = options?.engine ?? MotionEngine;
 
 	return (element: HTMLElement) => {
 		if (options) {
