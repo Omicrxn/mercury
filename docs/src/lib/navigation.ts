dcf53cef9fd4a9ad53aa6962278e847ed312bf73
Álex Ávila <alexavila0128@gmail.com>
2025-06-12 22:12:24 +0200

    chore: bumped package version

--- docs/src/lib/navigation.ts
+++ docs/src/lib/navigation.ts
@@ -7,36 +7,34 @@ import { getAllDocs } from "./utils.js";
 const allDocs = getAllDocs();
 
 const api = allDocs
-	.filter((doc) => doc.section === "API")
-	.map((doc) => ({
-		title: doc.title,
-		href: `/docs/${doc.slug}`,
-	}));
-
-
+  .filter((doc) => doc.section === "API")
+  .map((doc) => ({
+    title: doc.title,
+    href: `/docs/${doc.slug}`,
+  }));
 
 export const navigation = defineNavigation({
-	anchors: [
-		// {
-		// 	title: "Introduction",
-		// 	href: "/docs",
-		// 	icon: ChalkboardTeacher,
-		// },
-		{
-			title: "Getting Started",
-			href: "/docs/getting-started",
-			icon: RocketLaunch,
-		},
-		// {
-		// 	title: "Releases",
-		// 	href: "https://github.com/svecosystem/svecodocs/releases",
-		// 	icon: Tag,
-		// },
-	],
-	sections: [
-		{
-			title: "API",
-			items: api,
-		},
-	],
+  anchors: [
+    // {
+    // 	title: "Introduction",
+    // 	href: "/docs",
+    // 	icon: ChalkboardTeacher,
+    // },
+    {
+      title: "Getting Started",
+      href: "/docs/getting-started",
+      icon: RocketLaunch,
+    },
+    // {
+    // 	title: "Releases",
+    // 	href: "https://github.com/svecosystem/svecodocs/releases",
+    // 	icon: Tag,
+    // },
+  ],
+  sections: [
+    {
+      title: "API",
+      items: api,
+    },
+  ],
 });
