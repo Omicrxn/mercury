9bea3b7b7846fc026f9868a72c68bfde432c9764
Álex Ávila <alexavila0128@gmail.com>
2025-06-13 14:46:57 +0200

    [mercury] feat: gooey effect component

--- packages/src/lib/index.ts
+++ packages/src/lib/index.ts
@@ -1,6 +1,8 @@
 // Reexport your entry components here
 export { mercury } from './mercury/mercury.svelte.js';
 export { layout } from './mercury/layout.svelte.js';
-export {presence} from './mercury/animate-presence.js';
-export * from './mercury/animation-interface.js'
-export {MotionEngine, AnimeEngine, GSAPEngine} from './mercury/adapters/index.js'
+export { presence } from './mercury/animate-presence.js';
+export * from './mercury/animation-interface.js';
+export { MotionEngine, AnimeEngine, GSAPEngine } from './mercury/adapters/index.js';
+import GooeyEffect from './components/gooey-effect.svelte';
+export { GooeyEffect };
